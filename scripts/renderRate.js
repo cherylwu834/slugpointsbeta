5a080d17896a26b6c37e93c1ae807aa3196d0948
DancyPancy <jac.wong.k@gmail.com>
2023-02-24 14:33:59 -0800

    moved everything from broken version to beta

--- scripts/renderRate.js
+++ scripts/renderRate.js
@@ -1,6 +1,6 @@
 import { getDaysRemaining } from "./functions/daysRemaining.js";
 
-export function renderRate() {
+window['renderRate'] = function renderRate() {
     let userSlugPoints = document.querySelector("#userSlugPoints").value;
     let {diffDays, nextStart} = getDaysRemaining()
     document.getElementById('rate').innerHTML = (+userSlugPoints / diffDays).toFixed(2);
